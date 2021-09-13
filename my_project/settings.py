cb37b26c73cdd95f8f86313c19128800a77d53c9
komalkansara14 <krina.v.kansara@gmail.com>
2021-09-13 16:45:05 +0530

    database updated

--- my_project/settings.py
+++ my_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-96afub=&1$h5j1a&0yewwb2e)tdg0c*vws)qvv)(55(a+n^u-y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['first-ecapp.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['first-ecapp.herokuapp.com','localhost']
 
 
 # Application definition
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'new_db',
         'USER': 'root',
         'PASSWORD': '', 
-        'HOST': '',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
