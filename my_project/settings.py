dac734a6ea57c8f6a3cc0b6b067cc47f4c23e87f
komalkansara14 <krina.v.kansara@gmail.com>
2021-09-13 15:46:12 +0530

    settimgs file updated

--- my_project/settings.py
+++ my_project/settings.py
@@ -83,9 +83,6 @@ DATABASES = {
         'PASSWORD': '', 
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
     }
 }
 
